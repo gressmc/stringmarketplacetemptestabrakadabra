42c3254e17e23d1371221041c6a6b46650c20256
Roman Radetskiy <roman_radetskiy@epam.com>
2016-01-25 21:30:08 +0300

    up

--- src/main/java/grs/epam/data/model/Identified.java
+++
@@ -1,8 +0,0 @@
-package grs.epam.data.model;
-
-import java.io.Serializable;
-
-public interface Identified<PK extends Serializable> {
-    PK getId();
-    boolean isNew();
-}
--- src/main/webapp/WEB-INF/views/part/footer.jsp
+++
@@ -1,5 +0,0 @@
-<footer class="footer">
-    <div>
-        <p class="text-muted">Footer content will be here.</p>
-    </div>
-</footer>
